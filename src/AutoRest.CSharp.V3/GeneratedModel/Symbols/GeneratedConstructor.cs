c83928c2b0d488c85978a70f76042f29f91c6ee2
Pavel Krymets <pavel@krymets.com>
2019-11-21 10:29:24 -0800

    last one

--- src/AutoRest.CSharp.V3/GeneratedModel/Symbols/GeneratedConstructor.cs
+++ src/AutoRest.CSharp.V3/GeneratedModel/Symbols/GeneratedConstructor.cs
@@ -2,7 +2,7 @@
 {
     public class GeneratedConstructor : GeneratedMethod
     {
-        public GeneratedConstructor(Visibility visibility, string name) : base(visibility, MemberModifiers.None, null, name)
+        public GeneratedConstructor(Visibility visibility, string? name) : base(visibility, MemberModifiers.None, null, name)
         {
         }
     }
